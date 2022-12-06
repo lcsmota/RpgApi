97f215682b89af42ebe34a5d27e0b26fab7c97c8
lcsmota <sacul_m@protonmail.com>
2022-12-06 16:16:42 -0300

    Add model classes

---
+++ Models/Character.cs
@@ -0,0 +1,13 @@
+namespace RpgApi.Models;
+
+public class Character
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Race { get; set; } = string.Empty;
+    public double Height { get; set; }
+    public double Weight { get; set; }
+
+    public int RpgClassId { get; set; }
+    public RpgClass RpgClass { get; set; }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,8 @@
+namespace RpgApi.Models;
+
+public class RpgClass
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
