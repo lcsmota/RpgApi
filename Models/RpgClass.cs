5ebdfda03ba703eb9d58e1ca28f3628aa7daf1a8
lcsmota <sacul_m@protonmail.com>
2022-12-06 16:55:15 -0300

    Add list of characters

--- Models/RpgClass.cs
+++ Models/RpgClass.cs
@@ -5,4 +5,6 @@ public class RpgClass
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+
+    public List<Character> Characters { get; set; } = new List<Character>();
 }
