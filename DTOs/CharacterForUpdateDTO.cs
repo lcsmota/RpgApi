871cfdc12945e890b6383b58b12a2c34bb37bdcd
lcsmota <sacul_m@protonmail.com>
2022-12-06 17:05:42 -0300

    Add Character DTO for update

---
+++ DTOs/CharacterForUpdateDTO.cs
@@ -0,0 +1,11 @@
+namespace RpgApi.DTOs;
+
+public class CharacterForUpdateDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string Race { get; set; } = string.Empty;
+    public double Height { get; set; }
+    public double Weight { get; set; }
+
+    public int RpgClassId { get; set; }
+}
