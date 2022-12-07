4feefe82b5805834dc94d3d0ab804abbd256abf9
lcsmota <sacul_m@protonmail.com>
2022-12-07 10:33:07 -0300

    Add Character repository interface

---
+++ Interfaces/ICharacterRepository.cs
@@ -0,0 +1,13 @@
+using RpgApi.DTOs;
+using RpgApi.Models;
+
+namespace RpgApi.Interfaces;
+
+public interface ICharacterRepository
+{
+    Task<IEnumerable<Character>> GetCharactersAsync();
+    Task<Character> GetCharacterByIdAsync(int id);
+    Task AddCharacterAsync(Character character);
+    void UpdateCharacter(Character character);
+    void DeleteCharacter(Character character);
+}
