607291c48248603618b33e4149f4ad9601a63ee1
lcsmota <sacul_m@protonmail.com>
2022-12-07 10:33:27 -0300

    Add Character repository

---
+++ Repository/CharacterRepository.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using RpgApi.Context;
+using RpgApi.Interfaces;
+using RpgApi.Models;
+
+namespace RpgApi.Repository;
+
+public class CharacterRepository : ICharacterRepository
+{
+    private readonly RPGDbContext _rpgDbContext;
+    public CharacterRepository(RPGDbContext rpgDbContext)
+    {
+        _rpgDbContext = rpgDbContext;
+    }
+
+    public async Task<Character> GetCharacterByIdAsync(int id)
+    {
+        return await _rpgDbContext.Characters
+                                  .AsNoTracking()
+                                  .FirstOrDefaultAsync(prop => prop.Id == id);
+    }
+
+    public async Task<IEnumerable<Character>> GetCharactersAsync()
+    {
+        return await _rpgDbContext.Characters
+                                  .AsNoTracking()
+                                  .ToListAsync();
+    }
+    public async Task AddCharacterAsync(Character character)
+    {
+        await _rpgDbContext.Characters.AddAsync(character);
+    }
+
+    public void DeleteCharacter(Character character)
+    {
+        _rpgDbContext.Characters.Remove(character);
+    }
+
+    public void UpdateCharacter(Character character)
+    {
+        _rpgDbContext.Entry(character).State = EntityState.Modified;
+        _rpgDbContext.Update(character);
+    }
+}
