cc4c4463af827681cb83bbc4d88a955e6750de96
lcsmota <sacul_m@protonmail.com>
2022-12-08 10:39:20 -0300

    Add RPGClass controller

---
+++ Controllers/RPGClassesController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using RpgApi.Interfaces;
+
+namespace RpgApi.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class RPGClassesController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public RPGClassesController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetClassesAsync()
+    {
+        try
+        {
+            var classes = await _unitOfWork.RPGClassesRepository.GetRpgClasses();
+
+            return Ok(classes);
+        }
+        catch (System.Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult> GetClasseByIdAsync(int id)
+    {
+        try
+        {
+            var classe = await _unitOfWork.RPGClassesRepository.GetRpgClassByIdAsync(id);
+
+            if (classe is null) return NotFound("Class not found.");
+
+            return Ok(classe);
+        }
+        catch (System.Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
