14c9a19d668e69db68f85688cc2348764e5b4697
lcsmota <sacul_m@protonmail.com>
2022-12-09 21:48:13 -0300

    Add migrations InsertUser

--- Migrations/20221210004034_InsertUser.Designer.cs
+++ Migrations/20221210004034_InsertUser.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RpgApi.Context;
 
@@ -10,9 +11,11 @@ using RpgApi.Context;
 namespace RpgApi.Migrations
 {
     [DbContext(typeof(RPGDbContext))]
-    partial class RPGDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221210004034_InsertUser")]
+    partial class InsertUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20221210004034_InsertUser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RpgApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(80)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Login", "Password", "Role" },
+                values: new object[] { 1, "juca", "juca123!@#", "standard" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
