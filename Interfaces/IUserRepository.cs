d2c6e538e23d1f35729e3197cd441e5bc9abb00c
lcsmota <sacul_m@protonmail.com>
2022-12-09 22:41:42 -0300

    Add User repository interface

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+using RpgApi.Models;
+
+namespace RpgApi.Interfaces;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetUsersAsync();
+    Task<User> GetUserByIdAsync(int id);
+    Task AddUserAsync(User user);
+    void UpdateUser(User user);
+    void DeleteUser(User user);
+}
