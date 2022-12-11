c32fb812a0b526fdb004027be6fce2ba4ecee725
lcsmota <sacul_m@protonmail.com>
2022-12-10 21:50:16 -0300

    Add User repository interface on UOW

--- Repository/UnitOfWork.cs
+++ Repository/UnitOfWork.cs
@@ -8,6 +8,7 @@ public class UnitOfWork : IUnitOfWork
     private readonly RPGDbContext _context;
     private ICharacterRepository _characterRepo;
     private IRPGClassRepository _rpgClassRepo;
+    private IUserRepository _userRepo;
     public UnitOfWork(RPGDbContext context)
     {
         _context = context;
@@ -18,6 +19,9 @@ public class UnitOfWork : IUnitOfWork
 
     public IRPGClassRepository RPGClassesRepository
         => _rpgClassRepo ??= new RPGClassRepository(_context);
+
+    public IUserRepository UsersRepository
+        => _userRepo ??= new UserRepository(_context);
 
     public async Task CommitAsync()
     {
