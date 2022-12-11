0ccf6aa80294b7371099de2f3ba3a912216f9a68
lcsmota <sacul_m@protonmail.com>
2022-12-10 21:50:01 -0300

    Add User repository interface on UOW interface

--- Interfaces/IUnitOfWork.cs
+++ Interfaces/IUnitOfWork.cs
@@ -4,5 +4,6 @@ public interface IUnitOfWork
 {
     public ICharacterRepository CharactersRepository { get; }
     public IRPGClassRepository RPGClassesRepository { get; }
+    public IUserRepository UsersRepository { get; }
     public Task CommitAsync();
 }
